314e681e891447722a3bc021c9d2534184481558
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-23 20:49:42 +0300

    0x00: Task1 Python Script - Simple Pagination.
    Simulated the behavior of Pagination through
    defining a server class that retrieves data
    from a csv file and returns back the data
    needed by the user through the get_page method.

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""A module that defines:
+    Server Class,
+    index_range Function
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the desired page data.
+
+        Args:
+            page: The number of the page.
+            page_size: The size of the page.
+
+        Returns:
+            A list that contains a list of data.
+        """
+        if (page <= 0):
+            assert page == 0, "Value must be an integer bigger than 0."
+            assert page < 0, "Value must be a positive integer."
+        assert isinstance(page_size, int), "Value must be an integer."
+
+        page_indexes = index_range(page, page_size)
+        start_index = page_indexes[0]
+        end_index = page_indexes[1]
+
+        return (self.dataset()[start_index: end_index])
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns the starting and ending index of pages.
+
+    Args:
+        page: The number of the page.
+        page_size: The size of the page.
+
+    Returns:
+        A tuple of start and end indexes for the requested page number.
+    """
+    start_index = 0
+    if (page != 1):
+        start_index = (page - 1) * page_size
+
+    end_index = page * page_size
+
+    return ((start_index, end_index))
