395edef34733d2974f17527f702278e1b74ff31b
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-03 02:58:18 +0300

    0x02: Task1 Python Script - Babel Setup.

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""A module that defines a Flask App."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Defines the supported languages for translation.
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the language to be translated into.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home():
+    """Gets the Home Page.
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
