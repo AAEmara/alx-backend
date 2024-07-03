e7a15442ea356ad3d35ba5ec7fe86c3c5fe75b75
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-03 03:24:14 +0300

    0x02: Task2 Python Script - Rendering 2-index.html
    Rendering the right HTML file.

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -30,7 +30,7 @@ def get_locale():
 def home():
     """Gets the Home Page.
     """
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
