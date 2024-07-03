d0882831138db06e47c5feb89ce3e11e5ce41f7e
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-03 05:12:56 +0300

    0x02: Task4 Python Script and HTML Files.

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""A module that defines a Flask App."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+from typing import List
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """Defines the supported languages for translation.
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the language to be translated into.
+    """
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Gets the Home Page.
+    """
+    home_title: str = gettext(u'Welcome to Holberton')
+    home_header: str = gettext(u'Hello world!')
+    return render_template("4-index.html",
+                           home_title=home_title,
+                           home_header=home_header)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
