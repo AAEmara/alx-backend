17f79ecf02165a37037faaf96ecb13546c678317
AAEmara <abdelrahmanemara18@gmail.com>
2024-08-27 13:17:06 +0300

    0x03: Task1 JavaScript Script - Refactoring.
    Removed the funciton and used procedural code
    to run the redis-client.

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,13 +3,7 @@ import { createClient } from 'redis';
 const errMessage = 'Redis client not connected to the server:';
 const successMessage = 'Redis client connected to the server';
 
-async function connectRedis() {
-  const client = createClient();
+const client = createClient();
 
-  client.on('error', err => console.log(`${errMessage} ${err}`));
-  client.on('connect', () => console.log(successMessage));
-
-  await client.connect();
-}
-
-connectRedis();
+client.on('error', err => console.log(`${errMessage} ${err}`))
+  .on('connect', () => console.log(successMessage));
