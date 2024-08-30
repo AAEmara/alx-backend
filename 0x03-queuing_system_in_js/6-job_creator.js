5fe4d161e37875f1cb918a16bd22ea823c93c16d
AAEmara <abdelrahmanemara18@gmail.com>
2024-08-30 12:33:05 +0300

    0x03: Task7 JavaScript Script - Pub & Sub with Kue
    Used the same idea of PUBLISH and SUBSCRIBE
    commands in redis using queues and manage creating
    jobs on one node and processing the jobs on
    another node.

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ import { createQueue } from 'kue';
 
 const queue = createQueue();
 const jobData = {
-  phoneNumber: 'string',
-  message: 'string'
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account'
 };
 const job = queue.create('push_notification_code', jobData)
   .save( (err) => {
